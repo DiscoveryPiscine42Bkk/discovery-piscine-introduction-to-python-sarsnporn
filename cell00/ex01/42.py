8576b59dc0f182e1cb7a1ed3b5978f8863cd3bd1
sarsnporn <jasminnnn2112@gmail.com>
2025-06-17 05:52:17 +0000

    add name.py

--- cell00/ex01/42.py
+++ cell00/ex01/42.py
@@ -1,3 +1,5 @@
-print("42")
+first_name = "Saranporn"
+last_name = "Yornima"
+print (first_nam, last_name))
 
 
---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_namre = "Saranporn"
+last_name = "Yornima"
+print (first_namre, last_name)
